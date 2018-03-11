d7f4560751f3c32a6e01b62bf5eb832411ac43bc
excelero-andreas <31904878+excelero-andreas@users.noreply.github.com>
2018-03-10 21:04:46 -0800

    Update nvmesh_api_usage_example.py

--- nvmesh_api_usage_example.py
+++ nvmesh_api_usage_example.py
@@ -2,6 +2,38 @@
 # To make this excample tool work, you need to install the Requests Python package on the client/machine where you
 # intend to use it. The download information and instructions on how install the requests package can be found here:
 # http://docs.python-requests.org/en/master/user/install/#install
+#
+# Copyright (c) 2018 Excelero, Inc. All rights reserved.
+#
+# This Software is licensed under one of the following licenses:
+#
+# 1) under the terms of the "Common Public License 1.0" a copy of which is
+#    available from the Open Source Initiative, see
+#    http://www.opensource.org/licenses/cpl.php.
+#
+# 2) under the terms of the "The BSD License" a copy of which is
+#    available from the Open Source Initiative, see
+#    http://www.opensource.org/licenses/bsd-license.php.
+#
+# 3) under the terms of the "GNU General Public License (GPL) Version 2" a
+#    copy of which is available from the Open Source Initiative, see
+#    http://www.opensource.org/licenses/gpl-license.php.
+#
+# Licensee has the right to choose one of the above licenses.
+#
+# Redistributions of source code must retain the above copyright
+# notice and one of the license notices.
+#
+# Redistributions in binary form must reproduce both the above copyright
+# notice, one of the license notices in the documentation
+# and/or other materials provided with the distribution.
+#
+# Author:        Andreas Krause
+# Version:       in development
+# Maintainer:    Andreas Krause
+# Email:         andreas@excelero.com
+# 
+# Use at your own risk!
 
 # Importing the required packages
 import requests
@@ -17,8 +49,8 @@ requests.packages.urllib3.disable_warnings()
 # This is the default username and password intended for the use with the API set during the installation of the product
 # automatically. If this was changed after the installation, you have to call this tool with the -u and -p option to
 # set the correct credentials, username and/or password.
-username = "app@excelero.com"
-password = "admin"
+username = "<user name>"
+password = "<the secret password>"
 management_server = None
 management_server_port = "4000"
 api_protocol = None
